29b0ff1e4e1b71f0b8e2796f163e9a0a676d4cb3
марта <kukovamk@gmail.com>
2023-03-23 18:59:17 +0300

    HW_1

---
+++ HW_1/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+void Degree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+Degree(a, b);
